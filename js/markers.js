028c7a21a219678bbf528dbd8a08fd63399f6d48
Adam Spice <adamdidthis@hotmail.com>
2018-07-02 12:14:04 +1200

    updating site

---
+++ js/markers.js
@@ -0,0 +1,136 @@
+var myLocations = [{
+        name: "Media Design School",
+        address: "92 Albert St",
+        lat: -36.84802123789447,
+        lng: 174.76404469893995,
+        type: "University"
+    },
+    {
+        "name": "AA Head Office",
+        "address": "99 Albert St.",
+        "lat": -36.84868983944967,
+        "lng": 174.76341578648137,
+        "type": "Office",
+    },
+    {
+        "name": "Surf N Snow Backpackers",
+        "address": "102 Albert st",
+        "lat": -36.848521,
+        "lng": 174.763421,
+        "type": "Hostel",
+    },
+    {
+        "name": "Just Workout",
+        "address": "Ground Floor 12-26 Swanson St",
+        "lat": -36.84815515894017,
+        "lng": 174.7637773356503,
+        "type": "Gym",
+    },
+    {
+        "name": "Mai Thai",
+        "address": "57 Victoria St. W",
+        "lat": -36.84851906811102,
+        "lng": 174.76336363126308,
+        "type": "Thai Restaurant",
+    },
+    {
+        "name": "V&A Cafe",
+        "address": "98-102 Albert St.",
+        "lat": -36.84845111224448,
+        "lng": 174.76357100577098,
+        "type": "Café",
+    },
+    {
+        "name": "Nokia Auckland",
+        "address": "92 Albert Street",
+        "lat": -36.848077,
+        "lng": 174.763855,
+        "type": "Office",
+    },
+    {
+        "name": "Bus Stop 7097",
+        "address": "51 Victoria St.",
+        "lat": -36.84838011551084,
+        "lng": 174.76320147514343,
+        "type": "Bus Station",
+    },
+    {
+        "name": "FUGA Modern Japanese Brasserie",
+        "address": "62 Victoria Street West",
+        "lat": -36.848368863976305,
+        "lng": 174.76308757041505,
+        "type": "Japanese Restaurant",
+    },
+    {
+
+        "name": "AA Insurance (Head Office)",
+        "address": "46 Sale Street",
+        "lat": -36.84832857448825,
+        "lng": 174.76297030052214,
+        "type": "Insurance Office",
+    },
+    {
+        "name": "Da Vinci Restaurant",
+        "address": "87 Albert Street",
+        "lat": -36.84838918925349,
+        "lng": 174.763504626565,
+        "type": "Italian Restaurant",
+    },
+    {
+        "name": "Moana Road Art",
+        "address": "Westfield Downtown",
+        "lat": -36.84845854847103,
+        "lng": 174.7633374054711,
+        "type": "Art Gallery",
+    },
+    {
+        "name": "Kebab Time",
+        "address": "55 Victoria St. W",
+        "lat": -36.84851439368519,
+        "lng": 174.76320621802398,
+        "type": "Turkish Restaurant",
+    },
+    {
+
+        "name": "Mexican Cafe",
+        "address": "67 Victoria St. W",
+        "lat": -36.84847412573437,
+        "lng": 174.7630356519606,
+        "type": "Mexican Restaurant",
+    },
+    {
+        "name": "Tajmahal",
+        "address": "61 Victoria Street West",
+        "lat": -36.848335,
+        "lng": 174.763166,
+        "type": "Indian Restaurant",
+    },
+    {
+        "name": "Wongkok Cafe & Bar 旺角餐廳",
+        "address": "78 Victoria St W",
+        "lat": -36.8481079183926,
+        "lng": 174.761856500062,
+        "type": "Chinese Restaurant"
+    },
+    {
+        "name": "Avachi Fried Chicken",
+        "address": "291-297 Queen St",
+        "lat": -36.85002467000538,
+        "lng": 174.7648795235854,
+        "type": "Fried Chicken Joint"
+    },
+    {
+        "name": "Cafe Midnight Express",
+        "address": "59 Victoria St. W",
+        "lat": -36.848488051911495,
+        "lng": 174.7631492550378,
+        "type": "Turkish Restaurant"
+    },
+    {
+        "name": "EDENDZ - (formerly IANZ - International Academy of New Zealand)",
+        "address": "155 Queen Street",
+        "lat": -36.848213996519405,
+        "lng": 174.7637350998367,
+        "type": "General College & University",
+    }
+];
